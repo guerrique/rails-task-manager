e4b5bd0ca54595c6851b46b8f6fe568403e6c9cc
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-14 14:22:03 +0200

    refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index', as: :tasks
 
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
+  resources :tasks
 
-   get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks', to: 'tasks#index', as: :tasks
 
-   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-   patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
 
-   delete 'tasks/:id', to: 'tasks#destroy'
+  #  get 'tasks/:id', to: 'tasks#show', as: :task
 
+  #  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  #  patch 'tasks/:id', to: 'tasks#update'
+
+  #  delete 'tasks/:id', to: 'tasks#destroy'
 end
-
-
-  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  # post 'restaurants',    to: 'restaurants#create'
